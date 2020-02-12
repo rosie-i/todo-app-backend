a5129a141ec5530a6ababdda2a03bd53084de9f3
rosie-i <rosannainman@gmail.com>
2020-02-12 00:15:09 +0000

    Deleted unnecessary commented out code

--- handler.js
+++ handler.js
@@ -16,28 +16,4 @@ app.get('/tasks', function (req, res) {
   });
 });
 
-
-/* [
-  { id: uuidv1(), description: "Buy milk", due: "20/01/2020", completed: false },
-  { id: uuidv1(), description: "Hang out laundry", due: "21/01/2020", completed: false },
-  { id: uuidv1(), description: "Get a corgi", due: "22/01/2020", completed: false }
-]
-*/
-
 module.exports.tasks = serverless(app);
-
-// module.exports.tasks = async event => {
-//   return {
-//     statusCode: 200,
-//     body: JSON.stringify(
-//       {
-//         message: 'Woohoo! Your API is working!',
-//       },
-//       null,
-//       2
-//     ),
-//   };
-
-//   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-//   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-// };
