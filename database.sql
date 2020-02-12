dfedc049d08ec1c9dc9a204171c7f21f81f217ba
rosie-i <rosannainman@gmail.com>
2020-02-12 23:05:59 +0000

    Successful database querying via API + added mysqldump

---
+++ database.sql
@@ -0,0 +1,80 @@
+-- MySQL dump 10.13  Distrib 5.7.29, for Linux (x86_64)
+--
+-- Host: tr-course-rds-instance.c6mplnhevisd.eu-west-2.rds.amazonaws.com    Database: to_do_app
+-- ------------------------------------------------------
+-- Server version	5.7.22
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `task`
+--
+
+DROP TABLE IF EXISTS `task`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `task` (
+  `taskID` varchar(36) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `due` date DEFAULT NULL,
+  `completed` tinyint(1) DEFAULT NULL,
+  `userID` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`taskID`),
+  KEY `userID` (`userID`),
+  CONSTRAINT `task_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `task`
+--
+
+LOCK TABLES `task` WRITE;
+/*!40000 ALTER TABLE `task` DISABLE KEYS */;
+INSERT INTO `task` VALUES ('9e0674bf-4875-11ea-a183-0a3c2504a672','Finish Step Into Tech course','2020-03-19',0,1),('cfe199df-4875-11ea-a183-0a3c2504a672','Set up to-do list database with SQL','2020-02-06',1,1),('e8df38ec-486f-11ea-a183-0a3c2504a672','Get a corgi','2022-05-28',0,1);
+/*!40000 ALTER TABLE `task` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `userID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`userID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'Rosie Inman');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-02-12 22:54:49
--- handler.js
+++ handler.js
@@ -3,16 +3,53 @@
 const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
+const uuidv4 = require('uuid/v4');
+const mysql = require('mysql');
 
+const connection = mysql.createConnection({
+  host     : '********',
+  user     : '********',
+  password : '********',
+  database : '********'
+});
+
+// Retrieves tasks
 app.get('/tasks', function (req, res) {
 
-  
+  connection.query('SELECT * FROM `task` WHERE `userID` = "1"', function (error, results, fields) {
+    // error will be an Error if one occurred during the query
+    if(error) {
+      console.error("Your query had an issue with fetching tasks", error);
+      res.status(500).json({errorMessage: error})
+    }
+    else {
+      res.json({
+        tasks: results
+      });
+    }
+    // results will contain the results of the query
+    // fields will contain information about the returned results fields (if any)
+  });
+});
+
+// Creates tasks
+app.post('/tasks', function (req, res) {
   res.json({
-    tasks: [
-      { id: 1, description: "Buy milk", due: "20/01/2020", completed: false },
-      { id: 2, description: "Hang out laundry", due: "21/01/2020", completed: false },
-      { id: 3, description: "Get a corgi", due: "22/01/2020", completed: false }
-    ]
+    message: 'This is your POST function'
+  });
+});
+
+// Updates tasks
+app.put('/tasks/:id', function (req, res) {
+  res.json({
+    message: 'This is your PUT function'
+  });
+});
+
+// Deletes tasks
+app.delete('/tasks/:id', function (req, res) {
+  res.json({
+    message: 'This is your DELETE function'
   });
 });
 
