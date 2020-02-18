1ee1d68f857da99ebb3e7f7063f164c9cecd470b
rosie-i <rosannainman@gmail.com>
2020-02-18 20:43:30 +0000

    External config set up for DB connection

--- handler.js
+++ handler.js
@@ -7,10 +7,10 @@ const uuidv4 = require('uuid/v4');
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host     : '********',
-  user     : '********',
-  password : '********',
-  database : '********'
+  host     : process.env.DB_HOST,
+  user     : process.env.DB_USER,
+  password : process.env.DB_PASSWORD,
+  database : process.env.DB_SCHEMA
 });
 
 // Retrieves tasks
